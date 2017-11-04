096319bb7266822a051240f807b4207e9f1300dd
min-PC\min <minminprogramer131@gmail.com>
2017-11-04 17:49:04 +0630

    initialized skeleton project with default structure / value

---
+++ app/src/main/java/com/minhtetoo/PADCMMNEWS/SFCNewsApp.java
@@ -0,0 +1,16 @@
+package com.minhtetoo.PADCMMNEWS;
+
+import android.app.Application;
+
+/**
+ * Created by min on 11/4/2017.
+ */
+
+public class SFCNewsApp extends Application {
+    public static final  String LOG_TAG = "SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
