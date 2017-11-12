e3c49713d4d86e842f81e9c468e316f8b3254bd0
min-PC\min <minminprogramer131@gmail.com>
2017-11-12 18:23:03 +0630

    for min thein win

--- app/src/main/java/com/minhtetoo/PADCMMNEWS/SFCNewsApp.java
+++ app/src/main/java/com/minhtetoo/PADCMMNEWS/SFCNewsApp.java
@@ -2,9 +2,7 @@ package com.minhtetoo.PADCMMNEWS;
 
 import android.app.Application;
 
-/**
- * Created by min on 11/4/2017.
- */
+
 
 public class SFCNewsApp extends Application {
     public static final  String LOG_TAG = "SFCNewsApp";
